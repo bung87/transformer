be1b4465ec4f70ab887cbddd926c39638c76464b
zhoupeng <zhoupeng@xjgreat.com>
2020-04-17 19:02:05 +0800

    o

--- webpack.config.js
+++ webpack.config.js
@@ -30,28 +30,7 @@ if (isDevServer) {
 }
 
 module.exports = {
-  optimization: {
-    splitChunks: {
-      // chunks: 'async',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 6,
-      maxInitialRequests: 4,
-      automaticNameDelimiter: '~',
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true
-        }
-      }
-    }
-  },
+ 
   target: 'web',
   entry: path.resolve(__dirname, './src/app.tsx'),
   node: {
